3b380fb717596daea9dace3f863f6f945a7af65b
Alamin Islam <alamin890213@gmail.com>
2025-06-02 20:42:31 +0600

    db fix again

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/bg-remover`);
-    console.log("✅ MongoDB connected");
-  } catch (error) {
-    console.error("❌ MongoDB connection error:", error.message);
-    process.exit(1);
-  }
+
+    mongoose.connection.on('connected', () => {
+        console.log('mongodb cannected')
+    })
+  
+await mongoose.connect(`${process.env.MONGODB_URI}/bg-remover`)
+
+
 };
 
 export default connectDB;
